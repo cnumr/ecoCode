4ac4a571608298f99877a41978af10db5bd863fb
Tristan ROULET <tristan.roulet@viacesi.fr>
2022-02-25 15:03:19 +0100

    Rules' Checks Indentation (java & xml)

--- sonarqube-plugin-greenit/native-analyzer/android-plugin/src/test/files/leakage/CameraLeakCheckIssue.java
+++ sonarqube-plugin-greenit/native-analyzer/android-plugin/src/test/files/leakage/CameraLeakCheckIssue.java
@@ -1,17 +1,12 @@
 package android.hardware;
 
-
 public class Camera {
 
-    public void test(){
+    public void test() {
         open();// Noncompliant {{Failing to call android.hardware.Camera#release() can drain the battery in just a few hours.}}
     }
 
     public boolean open() {
         return true;
-    }
-
-    public void release() {
-
     }
 }
--- sonarqube-plugin-greenit/native-analyzer/android-plugin/src/test/files/leakage/MediaLeakMediaRecorderCheckIssue.java
+++ sonarqube-plugin-greenit/native-analyzer/android-plugin/src/test/files/leakage/MediaLeakMediaRecorderCheckIssue.java
@@ -1,17 +1,11 @@
 package android.media;
 
-
 public class MediaRecorder {
 
-    public void test(){
+    public void test() {
         MediaRecorder mr = new MediaRecorder(); // Noncompliant {{Failing to call release() on a Media Recorder may lead to continuous battery consumption.}}
     }
 
-    public MediaRecorder(){
-
-    }
-
-    public void release() {
-
+    public MediaRecorder() {
     }
 }
