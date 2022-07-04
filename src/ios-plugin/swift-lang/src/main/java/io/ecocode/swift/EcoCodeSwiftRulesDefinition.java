6db79755ed72ccdad920e18828e04426d21440e9
Gilles Grousset <gilles.grousset@insideapp.fr>
2022-07-04 15:47:49 +0200

    Swift plugin refactored to iOS plugin and modularized

---
+++ src/ios-plugin/swift-lang/src/main/java/io/ecocode/swift/EcoCodeSwiftRulesDefinition.java
@@ -0,0 +1,10 @@
+package io.ecocode.swift;
+
+import io.ecocode.rules.JSONRulesDefinition;
+
+public class EcoCodeSwiftRulesDefinition extends JSONRulesDefinition {
+
+    public EcoCodeSwiftRulesDefinition() {
+        super(Swift.REPOSITORY_KEY, Swift.REPOSITORY_NAME, Swift.KEY, Swift.RULES_PATH);
+    }
+}
--- src/swift-plugin/src/main/java/io/ecocode/EcoCodeSwiftRulesDefinition.java
+++
@@ -1,8 +0,0 @@
-package io.ecocode;
-
-public class EcoCodeSwiftRulesDefinition extends JSONRulesDefinition {
-
-    public EcoCodeSwiftRulesDefinition() {
-        super(Swift.REPOSITORY_KEY, Swift.REPOSITORY_NAME, Swift.KEY, Swift.RULES_PATH);
-    }
-}
