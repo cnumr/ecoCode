41d19401136422e59c4f8af99940e62c47da9319
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-09-21 11:40:09 +0200

    [Rule-S75] Removed useless code

--- src/java-plugin/src/main/java/fr/cnumr/java/checks/AvoidConcatenateStringsInLoop.java
+++ src/java-plugin/src/main/java/fr/cnumr/java/checks/AvoidConcatenateStringsInLoop.java
@@ -3,12 +3,10 @@ package fr.cnumr.java.checks;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
-import org.sonar.plugins.java.api.semantic.MethodMatchers;
 import org.sonar.plugins.java.api.tree.*;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Rule(
@@ -21,14 +19,6 @@ public class AvoidConcatenateStringsInLoop extends IssuableSubscriptionVisitor {
 
     protected static final String MESSAGERULE = "Don't concatenate Strings, use StringBuilder instead.";
 
-    private static final MethodMatchers STRING_METHOD = MethodMatchers.create()
-            .ofTypes("java.lang.String")
-            .anyName()
-            .withAnyParameters()
-            .build();
-
-    private final AvoidConcatenateStringsInLoopVisitor visitorInFile = new AvoidConcatenateStringsInLoopVisitor();
-
     @Override
     public List<Tree.Kind> nodesToVisit() {
         return Arrays.asList(
@@ -40,8 +30,7 @@ public class AvoidConcatenateStringsInLoop extends IssuableSubscriptionVisitor {
     @Override
     public void visitNode(@Nonnull Tree tree) {
         if (tree instanceof BinaryExpressionTree) {
-
-            BinaryExpressionTree binaryExpressionTree = (BinaryExpressionTree) tree;
+            final BinaryExpressionTree binaryExpressionTree = (BinaryExpressionTree) tree;
 
             if (binaryExpressionTree.leftOperand().symbolType().is("java.lang.String") ||
                     binaryExpressionTree.rightOperand().symbolType().is("java.lang.String")) {
@@ -49,28 +38,12 @@ public class AvoidConcatenateStringsInLoop extends IssuableSubscriptionVisitor {
             }
 
         } else if (tree instanceof AssignmentExpressionTree) {
-
-            AssignmentExpressionTree assignmentExpressionTree = (AssignmentExpressionTree) tree;
+            final AssignmentExpressionTree assignmentExpressionTree = (AssignmentExpressionTree) tree;
 
             if (assignmentExpressionTree.variable().symbolType().is("java.lang.String")) {
                 reportIssue(tree, MESSAGERULE);
             }
 
         }
-
-        tree.accept(visitorInFile);
-    }
-
-    private class AvoidConcatenateStringsInLoopVisitor extends BaseTreeVisitor {
-
-        @Override
-        public void visitMethodInvocation(@Nonnull MethodInvocationTree tree) {
-            if (STRING_METHOD.matches(tree)) {
-                reportIssue(tree, MESSAGERULE);
-            } else {
-                super.visitMethodInvocation(tree);
-            }
-        }
-
     }
 }
