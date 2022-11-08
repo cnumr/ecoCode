924aa6ade13e2b72a65cdda3ca9ee240949eebe8
oussamaLaribi <63214114+oussamaLaribi@users.noreply.github.com>
2022-11-08 14:15:38 +0100

    Fix sonar code smell

--- src/java-plugin/src/test/java/fr/cnumr/java/checks/AvoidMultipleIfElseStatementTest.java
+++ src/java-plugin/src/test/java/fr/cnumr/java/checks/AvoidMultipleIfElseStatementTest.java
@@ -3,7 +3,7 @@ package fr.cnumr.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-public class AvoidMultipleIfElseStatementTest {
+class AvoidMultipleIfElseStatementTest {
     @Test
     void test() {
         CheckVerifier.newVerifier()
